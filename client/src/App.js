51248d4328cde9c42074bc7f2069226c06adbc38
Alfredo <thealf154@gmail.com>
2021-06-19 13:22:57 -0500

    Succesful requests from the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,78 @@
-import logo from "./logo.svg";
-import "./App.css";
+// Dependencies
+import { React, useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import qs from "qs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Components
+import HomePage from "./components/homePage";
+import PersonalityPage from "./components/personalityPage";
+
+const App = () => {
+  const [topSongs, setTopSongs] = useState({});
+  const [topArtists, setTopArtists] = useState({});
+  const [token, setToken] = useState();
+
+  const handleLogIn = (logStatus) => {
+    if (logStatus) {
+      window.location = "http://localhost:3000/auth/login";
+    }
+  };
+
+  const cookieParser = (cookiename) => {
+    var cookiestring = RegExp(cookiename + "=[^;]+").exec(document.cookie);
+    if (cookiestring) {
+      cookiestring = cookiestring[0].replace(cookiename + "=", "");
+      return cookiestring;
+    } else return false;
+  };
+
+  useEffect(() => {
+    let token = cookieParser("accessToken");
+    setToken(token);
+    // Use API to get information if the token is available
+    if (token) {
+      const data = { accessToken: token };
+      const optionsArtists = {
+        method: "POST",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        url: "http://localhost:3000/getUsersTop/getTopArtists/",
+        data: qs.stringify(data),
+      };
+      const optionsSongs = {
+        method: "POST",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        url: "http://localhost:3000/getUsersTop/getTopSongs/",
+        data: qs.stringify(data),
+      };
+      console.log(token);
+      axios(optionsArtists)
+        .then((result) => {
+          setTopArtists(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      axios(optionsSongs)
+        .then((result) => {
+          setTopSongs(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+    }
+  }, []);
+
+  const firstPage = () => {
+    if (token) {
+      return <PersonalityPage topArtists={topArtists} topSongs={topSongs} />;
+    } else {
+      return <HomePage onLogIn={handleLogIn} />;
+    }
+  };
+
+  return <div>{firstPage()}</div>;
+};
 
 export default App;
---
+++ client/src/components/personalityPage.jsx
@@ -0,0 +1,9 @@
+// Dependencies
+import { React, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const PersonalityPage = (props) => {
+    return(<div>Hello</div>)
+}
+
+export default PersonalityPage;
