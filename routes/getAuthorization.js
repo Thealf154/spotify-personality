a22426b8b318632321e0ff4c3cf58682eae3fdac
Alfredo <thealf154@gmail.com>
2021-06-24 16:39:55 -0500

    Cookie

--- routes/getAuthorization.js
+++ routes/getAuthorization.js
@@ -98,7 +98,6 @@ app.get("/callback", function (req, res) {
           console.log(body);
         });
 
-        res.cookie("accessToken", access_token);
         res.cookie("accessToken", access_token, {
           domain: "http://localhost:3006/",
           secure: true,
